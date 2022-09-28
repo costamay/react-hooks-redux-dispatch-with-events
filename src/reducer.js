0743bb9552ab62d2e9e4ab5e741c3bbe17de76a6
costamay <titusouko@gmail.com>
2022-09-28 14:12:51 +0300

    add event listener

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,30 @@
 // add code snippets from README
+let state;
+
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "counter/increment":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+}
+
+
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+}
+
+function render(){
+    const app = document.getElementById("container");
+    app.textContent = state.count;
+ }
+
+dispatch({ type: '@@INIT' })
+
+let button = document.getElementById('button')
+
+button.addEventListener('click', () =>{
+    dispatch({type: "counter/increment"})
+})
